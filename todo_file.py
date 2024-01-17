d5dcbf70d5fe89574d3b971aaa3da20ed70bb87a
Dumitru Birsan <dbirsan@moldcell.intern>
2024-01-17 17:40:29 +0200

    deleted unnecessary images

--- todo_file.py
+++ todo_file.py
@@ -1,2 +1,3 @@
 #todo: function for create a .csv file with description of each column, that function should show column name and
 #      get from user short description, one by one until ends.
+# c
